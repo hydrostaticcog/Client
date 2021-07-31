3a5675ce659b1a9eab8f49cba900b87274819031
0/0 <zero@imaskeleton.me>
2021-07-30 19:15:51 -0600

    call the actual client entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// THIS FILE SHOULD NOT EVER BE CHANGED except for a few very specific circumstances
+// 1: adding command line argument parser
+// 2: adding config loader
+// otherwise everything goes to entrypoint()
+
+use client_core::entrypoint;
+
 fn main() {
-    println!("client")
+    entrypoint()
 }
